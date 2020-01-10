c4dfeaf283bf13575ecc83844797be5918e54da3
devansh071999 <devanshsampatjain@gmail.com>
2020-01-10 19:16:23 +0530

    home page added LoginRegister page added

---
+++ app/src/main/java/com/example/goa/StartActivity.java
@@ -0,0 +1,50 @@
+package com.example.goa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.goa.LoginActivity;
+import com.example.goa.MainActivity;
+import com.example.goa.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity {
+    Button login,register;
+    FirebaseUser firebaseUser;
+
+    protected void onStart() {
+        super.onStart();
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        //redirect if user is not null
+        if (firebaseUser!=null){
+            startActivity(new Intent(StartActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        login=findViewById(R.id.Login);
+        register=findViewById(R.id.Register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
