b85d2b4cf1aa19d32e6069f20902eaee360e4a86
BhuwanChandra <bhuwan7779@gmail.com>
2020-01-11 19:07:36 +0530

    changed side nav

--- app/src/main/java/com/example/goa/MainActivity.java
+++ app/src/main/java/com/example/goa/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.goa;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
